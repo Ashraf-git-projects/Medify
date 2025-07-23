e59690bae0ec97b94e87d54cf0bf2b8ece37dd4c
Ashraf <ashrafhussain2265@gmail.com>
2025-07-23 21:01:26 +0530

    HomeScreen layout/Content Complete

--- src/components/Advertise.jsx
+++ src/components/Advertise.jsx
@@ -2,22 +2,27 @@ import React from "react";
 import mobile from "../assets/mobile_screen.png";
 import g_play from "../assets/g_play.png";
 import apple from "../assets/app_store.png";
+import mob_cover from "../assets/mb_cvr.png"
 import './Advertise.css'
 
 export default function Advertise() {
   return (
     <div className="adv_container">
       <img src={mobile} className="mobile_img" alt="mobile_app"/>
+      <img src={mob_cover} className="mob_cover_img" alt="mobile_cvr"/>
       <div className="adv_texts">
         <h1>
-          Download the <span>Medify</span> App
+          Download the <br /><span style={{color:"#2AA7FF"}}>Medify</span> App
         </h1>
         <p>Get the Link to download the app</p>
         <form className="numb_form">
-          <label title="+91" />
-          <input type="number" />
+         <div className="phone_input_wrapper">
+    <span className="country_code">+91</span>
+    <input type="number" placeholder="Enter phone number" />
+  </div>
+          <button className="sms_btn">Send SMS</button>
         </form>
-        <button className="sms_btn">Send SMS</button>
+        
         <div className="app_imgs">
           <img src={g_play} alt="play store" />
           <img src={apple} alt="app store" />
--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -23,128 +23,81 @@ export default function Footer() {
         </div>
       </div>
       <div className="footer_treeone">
-        <Box className="footer_tree_box">
-          <SimpleTreeView>
-            <TreeItem
-              sx={{}}
-              className="footer_tree_itm"
-              itemId="grid"
-              label="Why choose our medical for your family?"
-            >
-              <TreeItem itemId="grid-community" label="Data-1" />
-              <TreeItem itemId="grid-pro" label="Data-2" />
-              <TreeItem itemId="grid-premium" label="Data-3" />
-            </TreeItem>
-            <TreeItem
-              sx={{ }}
-              className="footer_tree_itm"
-              itemId="pickers"
-              label="Why we are different from others?"
-            >
-              <TreeItem itemId="pickers-community" label="Data-1" />
-              <TreeItem itemId="pickers-pro" label="Data-1" />
-            </TreeItem>
-            <TreeItem
-              sx={{}}
-              className="footer_tree_itm"
-              itemId="charts"
-              label="Trusted & experience senior care & love"
-            >
-              <TreeItem itemId="charts-community" label="Data-1" />
-            </TreeItem>
-            <TreeItem
-              sx={{}}
-              className="footer_tree_itm"
-              itemId="tree-view"
-              label="How to get appointment for emergency cases?"
-            >
-              <TreeItem itemId="tree-view-community" label="Data-1" />
-            </TreeItem>
-          </SimpleTreeView>
-        </Box>
-      </div>
-      <div className="footer_treetwo">
-        <Box className="footer_tree_box">
-          <SimpleTreeView>
-            <TreeItem
-              sx={{}}
-              className="footer_tree_itm"
-              itemId="grid"
-              label="Why choose our medical for your family?"
-            >
-              <TreeItem itemId="grid-community" label="Data-1" />
-              <TreeItem itemId="grid-pro" label="Data-2" />
-              <TreeItem itemId="grid-premium" label="Data-3" />
-            </TreeItem>
-            <TreeItem
-              sx={{}}
-              className="footer_tree_itm"
-              itemId="pickers"
-              label="Why we are different from others?"
-            >
-              <TreeItem itemId="pickers-community" label="Data-1" />
-              <TreeItem itemId="pickers-pro" label="Data-1" />
-            </TreeItem>
-            <TreeItem
-              sx={{}}
-              className="footer_tree_itm"
-              itemId="charts"
-              label="Trusted & experience senior care & love"
-            >
-              <TreeItem itemId="charts-community" label="Data-1" />
-            </TreeItem>
-            <TreeItem
-              sx={{}}
-              className="footer_tree_itm"
-              itemId="tree-view"
-              label="How to get appointment for emergency cases?"
-            >
-              <TreeItem itemId="tree-view-community" label="Data-1" />
-            </TreeItem>
-          </SimpleTreeView>
-        </Box>
-      </div>
-      <div className="footer_treethree">
-        <Box className="footer_tree_box">
-          <SimpleTreeView>
-            <TreeItem
-              sx={{}}
-              className="footer_tree_itm"
-              itemId="grid"
-              label="Why choose our medical for your family?"
-            >
-              <TreeItem itemId="grid-community" label="Data-1" />
-              <TreeItem itemId="grid-pro" label="Data-2" />
-              <TreeItem itemId="grid-premium" label="Data-3" />
-            </TreeItem>
-            <TreeItem
-              sx={{ }}
-              className="footer_tree_itm"
-              itemId="pickers"
-              label="Why we are different from others?"
-            >
-              <TreeItem itemId="pickers-community" label="Data-1" />
-              <TreeItem itemId="pickers-pro" label="Data-1" />
-            </TreeItem>
-            <TreeItem
-              sx={{}}
-              className="footer_tree_itm"
-              itemId="charts"
-              label="Trusted & experience senior care & love"
-            >
-              <TreeItem itemId="charts-community" label="Data-1" />
-            </TreeItem>
-            <TreeItem
-              sx={{}}
-              className="footer_tree_itm"
-              itemId="tree-view"
-              label="How to get appointment for emergency cases?"
-            >
-              <TreeItem itemId="tree-view-community" label="Data-1" />
-            </TreeItem>
-          </SimpleTreeView>
-        </Box>
-      </div>  </div>
+  <Box className="footer_tree_box">
+    <SimpleTreeView>
+      <TreeItem className="footer_tree_itm" itemId="grid-1" label="About Us">
+        <TreeItem itemId="grid-community-1" label="Data-1" />
+        <TreeItem itemId="grid-pro-1" label="Data-2" />
+        <TreeItem itemId="grid-premium-1" label="Data-3" />
+      </TreeItem>
+      <TreeItem className="footer_tree_itm" itemId="pickers-1" label="Our Pricing">
+        <TreeItem itemId="pickers-community-1" label="Data-1" />
+        <TreeItem itemId="pickers-pro-1" label="Data-1" />
+      </TreeItem>
+      <TreeItem className="footer_tree_itm" itemId="charts-1" label="Our Gallery">
+        <TreeItem itemId="charts-community-1" label="Data-1" />
+      </TreeItem>
+      <TreeItem className="footer_tree_itm" itemId="tree-view-1" label="Appointment">
+        <TreeItem itemId="tree-view-community-1" label="Data-1" />
+      </TreeItem>
+      <TreeItem className="footer_tree_itm" itemId="view-1" label="Privacy Policy">
+        <TreeItem itemId="view-community-1" label="Data-1" />
+      </TreeItem>
+    </SimpleTreeView>
+  </Box>
+</div>
+
+<div className="footer_treetwo">
+  <Box className="footer_tree_box">
+    <SimpleTreeView>
+      <TreeItem className="footer_tree_itm" itemId="grid-2" label="Orthology">
+        <TreeItem itemId="grid-community-2" label="Data-1" />
+        <TreeItem itemId="grid-pro-2" label="Data-2" />
+        <TreeItem itemId="grid-premium-2" label="Data-3" />
+      </TreeItem>
+      <TreeItem className="footer_tree_itm" itemId="pickers-2" label="Neurology">
+        <TreeItem itemId="pickers-community-2" label="Data-1" />
+        <TreeItem itemId="pickers-pro-2" label="Data-1" />
+      </TreeItem>
+      <TreeItem className="footer_tree_itm" itemId="charts-2" label="Dental Care">
+        <TreeItem itemId="charts-community-2" label="Data-1" />
+      </TreeItem>
+      <TreeItem className="footer_tree_itm" itemId="tree-view-2" label="Opthalmology">
+        <TreeItem itemId="tree-view-community-2" label="Data-1" />
+      </TreeItem>
+      <TreeItem className="footer_tree_itm" itemId="ree-view-2" label="Cardiology">
+        <TreeItem itemId="tity-2" label="Data-1" />
+      </TreeItem>
+    </SimpleTreeView>
+  </Box>
+</div>
+
+<div className="footer_treethree">
+  <Box className="footer_tree_box">
+    <SimpleTreeView>
+      <TreeItem className="footer_tree_itm" itemId="grd-3" label="About Us">
+        <TreeItem itemId="rid-community-3" label="Data-1" />
+        <TreeItem itemId="rid-pro-3" label="Data-2" />
+        <TreeItem itemId="rid-premium-3" label="Data-3" />
+      </TreeItem>
+      <TreeItem className="footer_tree_itm" itemId="icers-3" label="Our Pricing">
+        <TreeItem itemId="pics-community-3" label="Data-1" />
+        <TreeItem itemId="prs-pro-3" label="Data-1" />
+      </TreeItem>
+      <TreeItem className="footer_tree_itm" itemId="carts-3" label="Our Gallery">
+        <TreeItem itemId="chascommunity-3" label="Data-1" />
+      </TreeItem>
+      <TreeItem className="footer_tree_itm" itemId="treeiew-3" label="Appointment">
+        <TreeItem itemId="viemmunity-3" label="Data-1" />
+      </TreeItem>
+      <TreeItem className="footer_tree_itm" itemId="iew-3" label="Privacy Policy">
+        <TreeItem itemId="unity-3" label="Data-1" />
+      </TreeItem>
+    </SimpleTreeView>
+  </Box>
+</div>
+ </div>
+      <hr style={{ width: 1300 ,border: 'none', opacity:0.2 , borderTop: '2px solid #ccc', margin: '16px 0' }} />
       <div className="footer_para">
         <p>Copyright ©2023 Surya Nursing Home.com. All Rights Reserved
             </p>
